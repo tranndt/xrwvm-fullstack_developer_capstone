c07f20c08b13237b9b618a1510d46f1b7376a0e3
tranndt <duythongvt1@gmail.com>
2024-08-09 20:03:12 -0400

    Implemented Django models

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -93,3 +93,16 @@ def registration(request):
 # Create a `add_review` view to submit a review
 # def add_review(request):
 # ...
+
+from .models import CarMake, CarModel
+
+def get_cars(request):
+    count = CarMake.objects.filter().count()
+    print(count)
+    if(count == 0):
+        initiate()
+    car_models = CarModel.objects.select_related('car_make')
+    cars = []
+    for car_model in car_models:
+        cars.append({"CarModel": car_model.name, "CarMake": car_model.car_make.name})
+    return JsonResponse({"CarModels":cars})
